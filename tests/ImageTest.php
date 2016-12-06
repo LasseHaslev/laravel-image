77762c56fc46c662858a52bbac5da925d25a2898
Lasse S. Haslev <lasse@haslev.no>
2016-12-06 10:24:37 +0100

    Added tests to write

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -7,4 +7,21 @@
 class ImageTest extends TestCase
 {
     // Can upload file Image::upload($filename, $parent);
+    // Can set what folder to store to
+    // Can set owner when uploading
+    // Is setting image info when uploading
+        // name
+        // size
+        // width
+        // type
+    // Can update image content $image->updateImage();
+    // Is keeping owner when updating
+    // Owner is referencing to id of class set in config
+    // cannot access owner if config owner is set to null
+    // Can set owner
+    // Can remove owner
+    // Can delete image
+    // Can get relative path of image
+    // Can get path of image
+    // Can get full path of image
 }
