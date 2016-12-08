ebc63f9ea897541c6d4cffcbcc33531f55d88cd5
Lasse S. Haslev <lasse@haslev.no>
2016-12-08 22:17:52 +0100

    Included Form Request

--- src/Http/Controllers/ImagesController.php
+++ src/Http/Controllers/ImagesController.php
@@ -8,6 +8,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use LasseHaslev\LaravelImage\Http\Requests\StoreImageRequest;
+use Illuminate\Http\Request;
 
 /**
  * Class ImagesController
